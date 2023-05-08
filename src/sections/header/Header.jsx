4959dc349d8fe98a2a4ac3df161d415d1578d18d
Tanner Hart <tannerhart24@yahoo.com>
2023-05-08 11:20:59 -0700

    Created and styled the header

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,9 +1,29 @@
+import HeaderImage from '../../assets/header.jpg';
 import './header.css';
+import data from './data';
 
 function Header() {
   return (
     <header id="header">
-      <h2>Header</h2>
+      <div className="container header__container">
+        <div className="header__profile">
+          <img src={HeaderImage} alt="Header Portrait" />
+        </div>
+        <h3>Tanner Hart</h3>
+        <p>
+          You are a click away from learning more about me and my work.
+        </p>
+        <div className="header__cta">
+          <a href="#contact" className='btn primary'>Let's Talk</a>
+          <a href="#portfolio" className='btn light'>My Work</a>
+        </div>
+
+        <div className="header__socials">
+          {
+            data.map((social) => <a key={social.id} href={social.link} target="_blank" rel="noreferrer">{social.icon}</a>)
+          }
+        </div>
+      </div>
     </header>
   )
 }
