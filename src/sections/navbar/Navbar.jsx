883bdb07d9947cc72e6b31a309133164cd84344b
Tanner Hart <tannerhart24@yahoo.com>
2023-05-08 11:20:48 -0700

    Created and styled nav bar

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,9 +1,24 @@
+import Logo from '../../assets/logo.jpg';
 import './navbar.css';
+import data from './data';
+import { IoIosColorPalette } from 'react-icons/io';
 
 function Navbar() {
   return (
     <nav>
-        <h2>Navbar</h2>
+        <div className="container nav__container">
+          <a href="index.html" className='nav__logo'>
+            <img src={Logo} alt="logo" />
+          </a>
+          <ul className='nav__menu'>
+            {
+              data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
+            }
+          </ul>
+          <button id='theme__icon'>
+            <IoIosColorPalette />
+          </button>
+        </div>
     </nav>
   )
 }
