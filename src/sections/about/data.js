9d3f512aec46100d4705e9b975854587f2be36e9
Tanner Hart <tannerhart24@yahoo.com>
2023-05-08 18:51:47 -0700

    Data file for experience tab

---
+++ src/sections/about/data.js
@@ -0,0 +1,11 @@
+import { FaAward } from 'react-icons/fa';
+import { TbBooks } from 'react-icons/tb';
+import { BiHappyHeartEyes } from 'react-icons/bi';
+
+const data = [
+    { id: 1, icon: <FaAward />, title: 'Experience', text: '3+ years Programming Experience' },
+    { id: 2, icon: <TbBooks />, title: 'Projects', text: '15+ Completed Projects' },
+    { id: 3, icon: <BiHappyHeartEyes />, title: 'Clients', text: 'Many happy clients' },
+];
+
+export default data;
