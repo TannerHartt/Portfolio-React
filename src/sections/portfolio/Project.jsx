be55db8b96adce51b7175cb358f614dd8ab3c58a
Tanner Hart <tannerhart24@yahoo.com>
2023-05-08 20:37:35 -0700

    Fixed typos

--- src/sections/portfolio/Project.jsx
+++ src/sections/portfolio/Project.jsx
@@ -1,23 +1,23 @@
 import Card from '../../components/Card/Card.jsx';
 
 const Project = ({ project }) => {
-  return (
-    <Card className="portfolio__project">
-        <div className="portfoio__project-image">
-            <img src={ project.image } alt="Project Img" />
-        </div>
-        <h4>{ project.title }</h4>
-        <p>{ project.desc }</p>
-        <div className="portfolio_project-cta">
-            <a href={ project.demo } className='btn sm' target='_blank' rel='noreferrer noopener'>
-                Demo
-            </a>
-            <a href={ project.github } className='btn sm primary' target='_blank' rel='noreferrer noopener'>
-                Github
-            </a>
-        </div>
-    </Card>
-  )
+    return (
+        <Card className="portfolio__project">
+            <div className="portfolio__project-image">
+                <img src={project.image} alt="Project Img" />
+            </div>
+            <h4>{project.title}</h4>
+            <p>{project.desc}</p>
+            <div className="portfolio__project-cta">
+                <a href={project.demo} className='btn sm' target='_blank' rel='noreferrer noopener'>
+                    Demo
+                </a>
+                <a href={project.github} className='btn sm primary' target='_blank' rel='noreferrer noopener'>
+                    Github
+                </a>
+            </div>
+        </Card>
+    )
 }
 
 export default Project
