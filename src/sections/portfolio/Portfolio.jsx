41298486354c0c8fa00abdf2e1ab32eabc3cdbbc
Tanner Hart <tannerhart24@yahoo.com>
2023-05-08 18:55:00 -0700

    Created component to display project categories and their corresponding projects

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -1,11 +1,24 @@
+import ProjectsCategories from './ProjectsCategories';
+import Projects from './Projects';
+import data from './data';
+import { useState } from 'react';
 import './portfolio.css';
 
 function Portfolio() {
+  const [projects, setProjects] = useState(data);
   return (
     <section id="portfolio">
-        <h2>Portfolio</h2>
+        <h2>Recent Projects</h2>
+        <p>
+          Check out some of the projects I have developed recently. Use the buttons to toggle the different categories.
+        </p>
+
+        <div className="container portfolio__container">
+          <ProjectsCategories />
+          <Projects projects={projects} />
+        </div>
     </section>
   )
 }
 
-export default Portfolio
+export default Portfolio;
