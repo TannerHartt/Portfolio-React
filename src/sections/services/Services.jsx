a4e4497673392a0ff8f84c3b730b3bd65b9436ea
Tanner Hart <tannerhart24@yahoo.com>
2023-05-08 18:52:42 -0700

    Created and styled services tab

--- src/sections/services/Services.jsx
+++ src/sections/services/Services.jsx
@@ -1,9 +1,27 @@
+import data from './data.js';
+import Card from '../../components/Card/Card.jsx';
 import './services.css';
 
 function Services() {
   return (
     <section id="services">
         <h2>Services</h2>
+        <p>I give you the best in all the serives below</p>
+        <div className="container services__container">
+          {
+            data.map((service) => {
+              return (
+                <Card key={service.id} className="service light">
+                  <div className="service__icon">{service.icon}</div>
+                  <div className="service__details">
+                    <h4>{service.title}</h4>
+                    <p>{service.text}</p>
+                  </div>
+                </Card>
+              );
+            })
+          }
+        </div>
     </section>
   )
 }
